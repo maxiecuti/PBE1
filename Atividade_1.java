28d0a6fd725c93599081ec8ad67acae55e24ffdf
maxie <159179953+maxiecuti@users.noreply.github.com>
2024-07-30 13:30:49 -0300

    Add files via upload

---
+++ Atividade_1.java
@@ -0,0 +1,20 @@
+package pogramacao_backend;
+
+import java.util.Scanner;
+
+public class Atividade_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite um número: ");
+		int x = sc.nextInt();
+		
+		System.out.print("Digite um número: ");
+		int y = sc.nextInt();
+		
+		System.out.println("A soma dos números é: " + (x+y));
+
+	}
+
+}
